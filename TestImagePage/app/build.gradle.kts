09368e21ff011b9e78fd88be0f46310636e5879a
kwj9002@gmail.com <150776867+kwj9002@users.noreply.github.com>
2024-01-29 11:26:18 +0900

    수정

--- TestImagePage/app/build.gradle.kts
+++ TestImagePage/app/build.gradle.kts
@@ -54,7 +54,5 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation("com.squareup.picasso:picasso:2.71828")
 }
